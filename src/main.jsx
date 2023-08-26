0d5399d7d0234983acaba5a7a43abb4ef29231bc
JuliaCMint <julia.malikova.jm@gmail.com>
2023-08-26 23:29:36 +0500

    import Redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { store } from "./store.jsx";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App tab='home' />
+    </React.StrictMode>
+  </Provider>
 );
