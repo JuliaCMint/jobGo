4503cc82057b073875a1a412f7770e490487b5e3
JuliaCMint <julia.malikova.jm@gmail.com>
2023-08-26 21:16:26 +0500

    add register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,93 @@
-const Register = () => {
-  return <div>Register</div>;
+import { styled } from "styled-components";
+import { Logo, FormRow } from "../components";
+import { useState, useEffect } from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isRegistered: true,
 };
+
+const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* name */}
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email */}
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password */}
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  align-items: center;
+  justify-content: center;
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+  .form {
+    max-width: 400px;
+    background-color: var(--white);
+  }
+
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+  .btn {
+    margin-top: 1rem;
+  }
+  .member-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--primary-dark);
+    cursor: pointer;
+    letter-spacing: var(--letterSpacing);
+  }
+`;
 
 export default Register;
