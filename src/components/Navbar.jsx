1eddd3e96ef82d71ecb8b06c4decc18096763f0d
JuliaCMint <julia.malikova.jm@gmail.com>
2023-08-29 21:00:32 +0500

    add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,132 @@
 import { styled } from "styled-components";
+import { BiChevronDown } from "react-icons/bi";
+import { BsLayoutSidebar } from "react-icons/bs";
+import { PiUserBold } from "react-icons/pi";
+import Logo from "./Logo";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  const [showLogout, setShowLogout] = useState(false);
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+
+  return (
+    <Wrapper>
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn' onClick={toggle}>
+          <BsLayoutSidebar />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            <PiUserBold />
+            {user?.name}
+            <BiChevronDown />
+          </button>
+          <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
+            <button
+              type='button'
+              className='dropdown-btn'
+              onClick={() => dispatch(clearStore("Logging out..."))}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  height: var(--nav-height);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .logo {
+    display: flex;
+    align-items: center;
+    width: 100px;
+  }
+  .nav-center {
+    display: flex;
+    width: 90vw;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .toggle-btn {
+    background: transparent;
+    border-color: transparent;
+    font-size: 1.75rem;
+    color: var(--primary-dark);
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+  }
+  background: var(--white);
+  .btn-container {
+    position: relative;
+  }
+  .btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0 0.5rem;
+    position: relative;
+  }
+
+  .dropdown {
+    position: absolute;
+    top: 40px;
+    left: 0;
+    width: 100%;
+    background: var(--primary-light);
+    /* padding: 0.5rem; */
+    text-align: center;
+    visibility: hidden;
+    border-radius: var(--borderRadiusSmall);
+  }
+  .show-dropdown {
+    visibility: visible;
+  }
+  .dropdown-btn {
+    background: transparent;
+    border-color: transparent;
+    color: var(--primary-dark);
+    letter-spacing: var(--letterSpacing);
+    text-transform: capitalize;
+    cursor: pointer;
+  }
+  .logo-text {
+    display: none;
+    margin: 0;
+  }
+  @media (min-width: 992px) {
+    position: sticky;
+    top: 0;
+
+    .nav-center {
+      width: 90%;
+    }
+    .logo {
+      display: none;
+    }
+    .logo-text {
+      display: block;
+    }
+  }
+`;
 
 export default Navbar;
