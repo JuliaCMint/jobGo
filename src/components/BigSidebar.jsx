24f9deb4d66ad328018aaa13faae8d9a359020f0
JuliaCMint <julia.malikova.jm@gmail.com>
2023-08-30 14:36:16 +0500

    add links, styles

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,6 +1,87 @@
+import { useSelector } from "react-redux";
 import { styled } from "styled-components";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 const BigSidebar = () => {
-  return <Wrapper>BigSidebar</Wrapper>;
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
-const Wrapper = styled.section``;
+const Wrapper = styled.aside`
+  display: none;
+  @media (min-width: 992px) {
+    display: block;
+    box-shadow: 1px 0px 0px 0px rgba(0, 0, 0, 0.1);
+    .sidebar-container {
+      background: var(--white);
+      min-height: 100vh;
+      height: 100%;
+      width: 250px;
+      margin-left: -250px;
+      transition: var(--transition);
+    }
+    .content {
+      position: sticky;
+      top: 0;
+    }
+    .show-sidebar {
+      margin-left: 0;
+    }
+    header {
+      height: 6rem;
+      display: flex;
+      align-items: center;
+      padding-left: 2.5rem;
+    }
+    .nav-links {
+      padding-top: 2rem;
+      display: flex;
+      flex-direction: column;
+    }
+    .nav-link {
+      display: flex;
+      align-items: center;
+      color: var(--grey-medium);
+      padding: 1rem 0;
+      padding-left: 2.5rem;
+      text-transform: capitalize;
+      transition: var(--transition);
+    }
+    .nav-link:hover {
+      background: var(--grey-extra-light);
+      padding-left: 3rem;
+      color: var(--primary-dark);
+    }
+    .nav-link:hover .icon {
+      color: var(--grey-dark);
+    }
+    .icon {
+      font-size: 1.5rem;
+      margin-right: 1rem;
+      display: grid;
+      place-items: center;
+      transition: var(--transition);
+    }
+    .active {
+      color: var(--grey-dark);
+    }
+    .active .icon {
+      color: var(--primary-dark);
+    }
+  }
+`;
 export default BigSidebar;
