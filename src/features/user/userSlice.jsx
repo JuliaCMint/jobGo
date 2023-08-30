bb6c64ee9a3ba26ce717667644f2f21e2ecbd961
JuliaCMint <julia.malikova.jm@gmail.com>
2023-08-30 19:02:56 +0500

    if 401 -> logout

--- src/features/user/userSlice.jsx
+++ src/features/user/userSlice.jsx
@@ -43,7 +43,10 @@ export const updateUser = createAsyncThunk(
       });
       return resp.data;
     } catch (error) {
-      console.log(error.response);
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue("Unauthorized! Logging out...");
+      }
       return thunkAPI.rejectWithValue(error.response.data.msg);
     }
   }
