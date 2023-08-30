20b175fb4c99b36769f206ac60c7cb4926e9a11b
JuliaCMint <julia.malikova.jm@gmail.com>
2023-08-30 20:49:49 +0500

    add rows, styles

--- src/pages/dashboard/AddJob.jsx
+++ src/pages/dashboard/AddJob.jsx
@@ -1,5 +1,153 @@
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { styled } from "styled-components";
+import FormRow from "../../components/FormRow";
+
 const AddJob = () => {
-  return <div>AddJob</div>;
+  const {
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+    editJobId,
+  } = useSelector((store) => store.job);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!position || !company || !jobLocation) {
+      toast.error("Please fill out all fields");
+      return;
+    }
+  };
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+  };
+
+  return (
+    <Wrapper>
+      <form className='form'></form>
+      <h3>{isEditing ? "edit job" : "add job"}</h3>
+      <div className='form-center'>
+        {/* position */}
+        <FormRow
+          type='text'
+          name='position'
+          value={position}
+          handleChange={console.log("position")}
+        />
+        {/* company */}
+        <FormRow
+          type='text'
+          name='company'
+          value={company}
+          handleChange={handleJobInput}
+        />
+        {/* jobLocation */}
+        <FormRow
+          type='text'
+          name='jobLocation'
+          labelText='job location'
+          value={jobLocation}
+          handleChange={handleJobInput}
+        />
+
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn btn-block clear-btn'
+            onClick={() => console.log("clear")}
+          >
+            clear
+          </button>
+          <button
+            type='submit'
+            className='btn btn-block submit-btn'
+            onClick={handleSubmit}
+            disabled={isLoading}
+          >
+            submit
+          </button>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  border-radius: var(--borderRadius);
+  width: 100%;
+  background: var(--white);
+  padding: 3rem 2rem 4rem;
+  h3 {
+    margin-top: 0;
+  }
+  .form {
+    margin: 0;
+    border-radius: 0;
+    box-shadow: none;
+    padding: 0;
+    max-width: 100%;
+    width: 100%;
+  }
+  .form-row {
+    margin-bottom: 0;
+  }
+  .form-center {
+    display: grid;
+    row-gap: 0.5rem;
+  }
+  .form-center button {
+    align-self: end;
+    height: 35px;
+    margin-top: 1rem;
+  }
+  .btn-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1rem;
+    align-self: flex-end;
+    margin-top: 0.5rem;
+    button {
+      height: 35px;
+    }
+  }
+  .clear-btn {
+    color: var(--primary-dark);
+    background-color: var(--white);
+    border-color: var(--grey-light);
+  }
+  .clear-btn:hover {
+    background: var(--primary-medium);
+    color: var(--white);
+    border-color: var(--primary-dark);
+  }
+  @media (min-width: 992px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+      column-gap: 1rem;
+    }
+    .btn-container {
+      margin-top: 0;
+    }
+  }
+  @media (min-width: 1120px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+    .form-center button {
+      margin-top: 0;
+    }
+  }
+`;
 
 export default AddJob;
