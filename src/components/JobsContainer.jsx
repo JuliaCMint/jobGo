6f620ded5f9c9b00ac95dc6c7f909f394b50a939
JuliaCMint <julia.malikova.jm@gmail.com>
2023-09-01 16:56:07 +0500

    set up components

--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -1,5 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
+import { styled } from "styled-components";
+import Job from "./Job";
+
 const JobsContainer = () => {
-  return <div>JobsContainer</div>;
+  const { jobs, isLoading } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  if (isLoading) {
+    return (
+      <Wrapper>
+        <h2>Loading</h2>
+      </Wrapper>
+    );
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <h5>jobs info</h5>
+      <div className='jobs'>
+        console.log(job) return <Job key={jobs._id} {...job} />
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section``;
 
 export default JobsContainer;
