6fa6b7c3266a04600ec05bf8c2cd49177e5abf0b
JuliaCMint <julia.malikova.jm@gmail.com>
2023-09-02 20:36:23 +0500

    get, dispay all jobs

--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -2,10 +2,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { styled } from "styled-components";
 import Job from "./Job";
 import Loading from "./Loading";
+import { useEffect } from "react";
+import { getAllJobs } from "../features/allJobs/allJobsSlice";
 
 const JobsContainer = () => {
   const { jobs, isLoading } = useSelector((store) => store.allJobs);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, []);
 
   if (isLoading) {
     return <Loading center />;
@@ -18,16 +24,39 @@ const JobsContainer = () => {
       </Wrapper>
     );
   }
+
   return (
     <Wrapper>
       <h5>jobs info</h5>
       <div className='jobs'>
-        console.log(job) return <Job key={jobs._id} {...job} />
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
       </div>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  margin-top: 4rem;
+  h2 {
+    text-transform: none;
+  }
+  & > h5 {
+    font-weight: 700;
+  }
+  .jobs {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 2rem;
+  }
+  @media (min-width: 992px) {
+    .jobs {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 1rem;
+    }
+  }
+`;
 
 export default JobsContainer;
