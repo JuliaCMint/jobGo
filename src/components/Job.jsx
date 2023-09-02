a1e46abe5322185ce2f8cacf20a2fda29b57fcf0
JuliaCMint <julia.malikova.jm@gmail.com>
2023-09-02 20:39:58 +0500

    set up single job component

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,5 +1,165 @@
-const Job = () => {
-  return <div>Job</div>;
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { styled } from "styled-components";
+import { PiMapPin, PiCalendarCheck, PiBriefcase } from "react-icons/pi";
+import JobInfo from "./JobInfo";
+
+const Job = ({
+  _id,
+  position,
+  company,
+  jobLocation,
+  jobType,
+  createdAt,
+  status,
+}) => {
+  const dispatch = useDispatch();
+
+  const date = createdAt;
+
+  return (
+    <Wrapper>
+      <header>
+        <div className='main-icon'>{company.charAt(0)}</div>
+        <div className='info'>
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className='content'>
+        <div className='content-center'>
+          <JobInfo icon={<PiMapPin />} text={jobLocation} />
+          <JobInfo icon={<PiCalendarCheck />} text={date} />
+          <JobInfo icon={<PiBriefcase />} text={jobType} />
+          <div className={`status ${status}`}>{status}</div>
+        </div>
+        <footer>
+          <div className='actions'>
+            <Link
+              to='/add-job'
+              className='btn edit-btn'
+              onClick={() => {
+                console.log("edit job");
+              }}
+            >
+              Edit
+            </Link>
+            <button
+              type='button'
+              className='btn delete-btn'
+              onClick={() => {
+                console.log("delete job");
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </footer>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.article`
+  background: var(--white);
+  border-radius: var(--borderRadius);
+  display: grid;
+  grid-template-rows: 1fr auto;
+
+  header {
+    padding: 1rem 1.5rem;
+    border-bottom: 1px solid var(--grey-extra-light);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+    h5 {
+      letter-spacing: 0;
+    }
+  }
+  .main-icon {
+    width: 60px;
+    height: 60px;
+    display: grid;
+    place-items: center;
+    background: var(--primary-dark);
+    border-radius: var(--borderRadius);
+    font-size: 1.5rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: var(--white);
+    margin-right: 2rem;
+  }
+  .info {
+    h5 {
+      margin-bottom: 0.25rem;
+    }
+    p {
+      margin: 0;
+      text-transform: capitalize;
+      color: var(--grey-dark);
+      letter-spacing: var(--letterSpacing);
+    }
+  }
+  .pending {
+    background: #fcefc7;
+    color: #e9b949;
+  }
+  .interview {
+    background: #e0e8f9;
+    color: #647acb;
+  }
+  .declined {
+    color: #d66a6a;
+    background: #ffeeee;
+  }
+  .content {
+    padding: 1rem 1.5rem;
+  }
+  .content-center {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 0.5rem;
+    @media (min-width: 576px) {
+      grid-template-columns: 1fr 1fr;
+    }
+    @media (min-width: 992px) {
+      grid-template-columns: 1fr;
+    }
+    @media (min-width: 1120px) {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  .status {
+    border-radius: var(--borderRadius);
+    text-transform: capitalize;
+    letter-spacing: var(--letterSpacing);
+    text-align: center;
+    width: 100px;
+    height: 30px;
+    margin-top: 0.5rem;
+  }
+  footer {
+    margin-top: 1rem;
+  }
+  .edit-btn,
+  .delete-btn {
+    letter-spacing: var(--letterSpacing);
+    cursor: pointer;
+    height: 30px;
+  }
+  .edit-btn {
+    color: var(--green-dark);
+    background: var(--green-light);
+    margin-right: 0.5rem;
+  }
+  .delete-btn {
+    color: var(--red-dark);
+    background: var(--red-light);
+  }
+  &:hover .actions {
+    visibility: visible;
+  }
+`;
 
 export default Job;
