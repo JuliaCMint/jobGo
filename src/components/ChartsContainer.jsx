6abfd28dd5f5cdee49ed3ae15e75bd262cb3a319
JuliaCMint <julia.malikova.jm@gmail.com>
2023-09-05 21:38:28 +0500

    add charts

--- src/components/ChartsContainer.jsx
+++ src/components/ChartsContainer.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+import BarChart from "./BarChartComponent";
+import AreaChart from "./AreaChartComponent";
+import { useSelector } from "react-redux";
+
 const ChartsContainer = () => {
-  return <h2>ChartsContainer</h2>;
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useSelector((store) => store.allJobs);
+  return (
+    <Wrapper>
+      <h3>Monthly Applications</h3>
+      <button
+        type='button'
+        onClick={() => {
+          setBarChart(!barChart);
+        }}
+      >
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.article``;
 
 export default ChartsContainer;
