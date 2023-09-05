3ba84428474f7d092a02e565f56c9a099494c101
JuliaCMint <julia.malikova.jm@gmail.com>
2023-09-05 14:18:05 +0500

    stats items in stats container

--- src/components/StatsContainer.jsx
+++ src/components/StatsContainer.jsx
@@ -1,5 +1,55 @@
+import { useSelector } from "react-redux";
+import StatItem from "./StatItem";
+import { styled } from "styled-components";
+import { MdCheckCircleOutline, MdSchedule, MdCancel } from "react-icons/md";
+
 const StatsContainer = () => {
-  return <h2>StatsContainer</h2>;
+  const { stats } = useSelector((store) => store.allJobs);
+
+  const defaultStats = [
+    {
+      title: "pending applications",
+      count: stats.pending || 0,
+      icon: <MdCheckCircleOutline />,
+      color: "#f2bd2c",
+      bcg: "#fae5ab",
+    },
+    {
+      title: "interviews scheduled",
+      count: stats.interview || 0,
+      icon: <MdSchedule />,
+      color: "#00d563",
+      bcg: "#ccf7e0",
+    },
+    {
+      title: "jobs declined",
+      count: stats.declined || 0,
+      icon: <MdCancel />,
+      color: "#db3754",
+      bcg: "#f3cbd2",
+    },
+  ];
+
+  return (
+    <Wrapper>
+      {defaultStats.map((item, index) => {
+        return <StatItem key={index} {...item} />;
+      })}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  display: grid;
+  row-gap: 2rem;
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1rem;
+  }
+  @media (min-width: 1120px) {
+    grid-template-columns: 1fr 1fr 1fr;
+    column-gap: 1rem;
+  }
+`;
 
 export default StatsContainer;
