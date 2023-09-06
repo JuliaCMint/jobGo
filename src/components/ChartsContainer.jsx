02465a3e96b80547b3cb3b2056f3725ba180cf22
JuliaCMint <julia.malikova.jm@gmail.com>
2023-09-06 19:42:07 +0500

    add styles

--- src/components/ChartsContainer.jsx
+++ src/components/ChartsContainer.jsx
@@ -23,6 +23,21 @@ const ChartsContainer = () => {
   );
 };
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  margin-top: 4rem;
+  text-align: center;
+  button {
+    background: transparent;
+    border-color: transparent;
+    text-transform: capitalize;
+    color: var(--primary-500);
+    font-size: 1.25rem;
+    cursor: pointer;
+  }
+  h4 {
+    text-align: center;
+    margin-bottom: 0.75rem;
+  }
+`;
 
 export default ChartsContainer;
