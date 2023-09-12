9a7dd4b9a57df6117bd6a47034eb15fe7a3a40dc
JuliaCMint <julia.malikova.jm@gmail.com>
2023-09-12 15:02:02 +0500

    setup pages

--- src/components/PageBtnContainer.jsx
+++ src/components/PageBtnContainer.jsx
@@ -1,7 +1,96 @@
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import { useSelector, useDispatch } from "react-redux";
+import styled from "styled-components";
 
 const PageBtnContainer = () => {
-  return <div>PageBtnContainer</div>;
+  const { numOfPages, page } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+
+  const nextPage = () => {};
+  const prevPage = () => {};
+
+  return (
+    <Wrapper>
+      <button className='prev-btn' onClick={prevPage}>
+        <AiOutlineLeft />
+        prev
+      </button>
+      <div className='btn-container'>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              type='button'
+              className={pageNumber === page ? "pageBtn active" : "pageBtn"}
+              key={pageNumber}
+              onClick={() => console.log("change page")}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button className='next-btn' onClick={nextPage}>
+        next
+        <AiOutlineRight />
+      </button>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  height: 6rem;
+  margin-top: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  flex-wrap: wrap;
+  gap: 1rem;
+  .btn-container {
+    background: var(--primary-100);
+    border-radius: var(--borderRadius);
+  }
+  .pageBtn {
+    background: transparent;
+    border-color: transparent;
+    width: 50px;
+    height: 40px;
+    font-weight: 700;
+    font-size: 1.25rem;
+    color: var(--primary-500);
+    transition: var(--transition);
+    border-radius: var(--borderRadius);
+    cursor: pointer;
+  }
+  .active {
+    background: var(--primary-500);
+    color: var(--white);
+  }
+  .prev-btn,
+  .next-btn {
+    width: 100px;
+    height: 40px;
+    background: var(--white);
+    border-color: transparent;
+    border-radius: var(--borderRadius);
+    color: var(--primary-500);
+    text-transform: capitalize;
+    letter-spacing: var(--letterSpacing);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    cursor: pointer;
+    transition: var(--transition);
+  }
+  .prev-btn:hover,
+  .next-btn:hover {
+    background: var(--primary-500);
+    color: var(--white);
+  }
+`;
 
 export default PageBtnContainer;
