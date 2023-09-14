7a13681cadc1078e5086a8c8734f3cad2013ab2e
JuliaCMint <julia.malikova.jm@gmail.com>
2023-09-14 20:30:45 +0500

    edit chart layout, colors; make responsive

--- src/components/PieChartComponent.jsx
+++ src/components/PieChartComponent.jsx
@@ -1,50 +1,38 @@
 import { styled } from "styled-components";
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 
-const COLORS = ["#f2bd2c", "#00d563", "#db3754"];
+const COLORS = ["#6136bf", "#41EAD4", "#F71735"];
 
 const PieChartComponent = ({ data }) => {
   return (
-    <Wrapper>
-      <PieChart width={200} height={300}>
-        <Pie
-          data={data}
-          cx={100}
-          cy={150}
-          innerRadius={50}
-          outerRadius={60}
-          // fill='#6136bf'
-          paddingAngle={5}
-          dataKey='count'
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-        {/* <Pie
-        data={data}
-        cx={420}
-        cy={200}
-        startAngle={180}
-        endAngle={0}
-        innerRadius={60}
-        outerRadius={80}
-        // fill='#00d563'
-        paddingAngle={5}
-        dataKey='count'
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie> */}
-      </PieChart>
-    </Wrapper>
+    <div style={{ width: "300px", height: 300 }}>
+      <ResponsiveContainer>
+        <PieChart width={800} height={400}>
+          <Pie
+            data={data}
+            cx={150}
+            cy={150}
+            innerRadius={65}
+            outerRadius={70}
+            // fill='#6136bf'
+            paddingAngle={1}
+            dataKey='count'
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
 const Wrapper = styled.article`
-  /* padding: 2rem; */
-  background: var(--white);
+  background: transparent;
   border-radius: var(--borderRadiusSmall);
 `;
 
