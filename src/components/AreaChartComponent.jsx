1b44a28177b415dfe5e507972a5e96aae643a27c
JuliaCMint <julia.malikova.jm@gmail.com>
2023-09-14 21:09:21 +0500

    add color gradient

--- src/components/AreaChartComponent.jsx
+++ src/components/AreaChartComponent.jsx
@@ -11,11 +11,22 @@ const AreaChartComponent = ({ data }) => {
   return (
     <ResponsiveContainer width='100%' height={300}>
       <AreaChart data={data} margin={{ top: 50 }}>
+        <defs>
+          <linearGradient id='colorUv' x1='0' y1='0' x2='0' y2='1'>
+            <stop offset='5%' stopColor='#6136bf' stopOpacity={1.0} />
+            <stop offset='95%' stopColor='#6136bf' stopOpacity={0} />
+          </linearGradient>
+        </defs>
         <CartesianGrid strokeDasharray='3 3' />
         <XAxis dataKey='date' />
         <YAxis allowDecimals={false} />
         <Tooltip />
-        <Area type='monotone' dataKey='count' stroke='#6136bf' fill='#815ecc' />
+        <Area
+          type='monotone'
+          dataKey='count'
+          stroke='#442686'
+          fill='url(#colorUv)'
+        />
       </AreaChart>
     </ResponsiveContainer>
   );
