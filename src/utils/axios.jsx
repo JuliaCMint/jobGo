f850de861e0b04c13304530f799bb13c74b773ce
JuliaCMint <julia.malikova.jm@gmail.com>
2024-06-24 13:31:25 +0500

    updated url

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -3,7 +3,7 @@ import { getUserFromLocalStorage } from "./localStorage";
 import { clearStore } from "../features/user/userSlice";
 
 const customFetch = axios.create({
-  baseURL: "https://jobify-prod.herokuapp.com/api/v1/toolkit",
+  baseURL: "https://redux-toolkit-jobster-api-server.onrender.com/api/v1",
 });
 
 customFetch.interceptors.request.use((config) => {
